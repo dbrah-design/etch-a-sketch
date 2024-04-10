513caedc3c72d34e0565f0d66f1392ec2e2f021f
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2024-04-10 10:28:43 -0400

    Added a function to generate a random color

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const gridSquare = document.createElement("div");
 gridSquare.classList.add("grid-square");
 
 container.appendChild(gridSquare)
+
+
+function generateColor(){
+  const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  let code = "";
+  for(let i=0; i<6; i++){
+   code += hexArray[Math.floor(Math.random()*16)];
+  }
+  return `#${code}`
+ }
