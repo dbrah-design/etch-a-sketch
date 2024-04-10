496b660cd7809c2f1e5a849cc34a836427a96a8e
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2024-04-10 09:40:48 -0400

    Added logic to create one grid square

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".grid-container");
+
+const gridSquare = document.createElement("div");
+gridSquare.classList.add("grid-square");
+
+container.appendChild(gridSquare)
