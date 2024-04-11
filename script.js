a4852ca66a163f7fd51e0ce38e74451fc4d3e1a0
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2024-04-11 15:15:37 -0400

    Made function for test btn to generate squares in grid

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const container = document.querySelector(".grid-container");
+const generateBtn = document.querySelector(".test-btn")
 
+
+function generateSquare() {
+  const gridSquare = document.createElement("div");
+  gridSquare.classList.add("grid-square");
+  gridSquare.style.backgroundColor = generateColor()
+  container.appendChild(gridSquare)
+}
+
+generateBtn.addEventListener("click", generateSquare)
+/*
 const gridSquare = document.createElement("div");
 gridSquare.classList.add("grid-square");
 gridSquare.style.backgroundColor = generateColor()
 
 container.appendChild(gridSquare)
-
+*/
 
 function generateColor(){
   const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
