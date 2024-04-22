c6277320964721be841e395391d60ce5b4dee87a
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2024-04-22 19:39:30 -0400

    Moved mouse over event listener into generate grid function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gridBtn = document.querySelector(".grid-btn");
 
 function generateGrid(size) {
   gridContainer.replaceChildren()
+  
   for( i = 0; i < size * size; i++) {
     const squareLength = (500 - 2 * 2) / size;
     const div = document.createElement("div");
@@ -11,6 +12,16 @@ function generateGrid(size) {
     div.style.height = `${squareLength}px`
     gridContainer.appendChild(div);
   }
+
+  const gridSquares = document.querySelectorAll(".grid-square");
+
+  gridSquares.forEach((square) => {
+  
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = generateColor()
+    });
+  
+  });
 }
 
 generateGrid(16)
@@ -24,17 +35,6 @@ gridBtn.addEventListener("click", () => {
   }
   
 })
-
-const gridSquares = document.querySelectorAll(".grid-square");
-
-gridSquares.forEach((square) => {
-  
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = generateColor()
-  });
-  
-});
-
 
 function generateColor(){
   const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
